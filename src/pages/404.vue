3a7218fe888c1eb94159905b90435f3cf1cd6655
Shin72111 <tam.tran72111@gmail.com>
2019-10-20 19:21:08 -0400

    Add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,31 @@
+<template>
+  <Layout>
+    <header class="error">
+      <Banner title="oops it's a dead end">
+        <g-link to="/" class="btn-white">
+          back to home page
+        </g-link>
+      </Banner>
+    </header>
+  </Layout>
+</template>
+
+<script>
+import Banner from '@/components/Banner'
+
+export default {
+  components: {
+    Banner
+  }
+}
+</script>
+
+<style scoped>
+.error {
+  background: var(--primaryColor);
+  min-height: calc(100vh - 62px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
